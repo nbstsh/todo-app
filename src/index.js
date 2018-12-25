aa2317f39eedb821a623c56a4960f84258e598b3
Satoshi Inaba <satoshi@SatoshinoMacBook-puro.local>
2018-12-26 00:46:13 +0700

    add text filtering event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import { getTodos, saveTodos, createTodo, toggleTodo, removeTodo } from './todo'
+import { getFilters, setFilters, getFilteredTodos } from './filter'
+import { generateSummaryDOM, generateTodoDOM, renderTodos } from './view'
+
+renderTodos()
+
+// searchtext filtering
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    setFilters({ searchText: e.target.value })
+    renderTodos()
+})
